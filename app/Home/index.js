62e1543b3e016f6e909b63e5d917dab9c76084a0
Eduardo Mendes <68574046+eduardobmendex@users.noreply.github.com>
2024-10-03 16:29:58 -0300

    Update index.js

--- app/Home/index.js
+++ app/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Button, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import TaskList from '../../components/TaskList';
 import TaskModal from '../../components/TaskModal';
@@ -9,13 +9,14 @@ const STORAGE_KEY = '@tasks';
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
+  const [newTaskText, setNewTaskText] = useState(""); // Novo estado para armazenar o texto da nova tarefa
   const [isModalVisible, setModalVisible] = useState(false);
   const [taskToEdit, setTaskToEdit] = useState(null);
   const [username, setUsername] = useState("");
   const [completedTasks, setCompletedTasks] = useState(0);
   const navigation = useNavigation();
 
-   const fetchUserData = async () => {
+  const fetchUserData = async () => {
     try {
       const user = await AsyncStorage.getItem("usuario");
       if (user) {
@@ -27,7 +28,7 @@ const Home = () => {
     }
   };
 
-   const loadTasks = async () => {
+  const loadTasks = async () => {
     try {
       const jsonTasks = await AsyncStorage.getItem(STORAGE_KEY);
       if (jsonTasks) {
@@ -50,7 +51,7 @@ const Home = () => {
     fetchUserData();
   }, []);
 
-   const saveTasks = async (tasks) => {
+  const saveTasks = async (tasks) => {
     try {
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
     } catch (error) {
@@ -58,19 +59,25 @@ const Home = () => {
     }
   };
 
-   const addTask = (task) => {
-    const newTasks = [...tasks, { 
-      id: Date.now().toString(), 
-      text: task, 
+  const addTask = () => {
+    if (newTaskText.trim() === "") {
+      alert("Por favor, insira uma tarefa.");
+      return;
+    }
+
+    const newTasks = [...tasks, {
+      id: Date.now().toString(),
+      text: newTaskText, // Usa o texto do campo de input
       completed: false,
-      creationDate: new Date().toISOString() 
+      creationDate: new Date().toISOString()
     }];
+
     setTasks(newTasks);
     saveTasks(newTasks);
-    setModalVisible(false);
+    setNewTaskText(""); // Limpa o campo de input após a adição
   };
 
-   const toggleTaskCompletion = (id) => {
+  const toggleTaskCompletion = (id) => {
     const newTasks = tasks.map(task => (task.id === id ? { ...task, completed: !task.completed } : task));
     setTasks(newTasks);
     saveTasks(newTasks);
@@ -78,29 +85,63 @@ const Home = () => {
     setCompletedTasks(completedCount);
   };
 
-   const deleteTask = (id) => {
+  const deleteTask = (id) => {
     const newTasks = tasks.filter(task => task.id !== id);
     setTasks(newTasks);
     saveTasks(newTasks);
     alert('Task deletada.')
   };
 
-   const openEditModal = (task) => {
+  const openEditModal = (task) => {
     setTaskToEdit(task);
     setModalVisible(true);
   };
 
-   const editTask = (id, updatedText) => {
+  const editTask = (id, updatedText) => {
     const updatedTasks = tasks.map(task =>
       task.id === id ? { ...task, text: updatedText } : task
     );
     setTasks(updatedTasks);
     saveTasks(updatedTasks);
-    setTaskToEdit(null); 
+    setTaskToEdit(null);
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.containerText}>
+      <Text style={styles.usernameText}>Olá, {username}!</Text>
+
+      </View>
+
+      <Text style={{fontWeight:500, fontSize:20, paddingTop:20}}>Cadastrar uma tarefa:</Text>
+
+<View style={styles.contentSave} >
+      <TextInput
+        style={styles.input}
+         value={newTaskText}
+        onChangeText={setNewTaskText}
+      />
+
+      <TouchableOpacity style={styles.addButton} onPress={addTask}>
+        <Text style={styles.addButtonText}>Salvar</Text>
+      </TouchableOpacity>
+      </View>
+      <TaskList 
+      
+        tasks={tasks} 
+        toggleTaskCompletion={toggleTaskCompletion} 
+        deleteTask={deleteTask} 
+        openEditModal={openEditModal} 
+      />
+      
+      <TaskModal
+    
+        isVisible={isModalVisible}
+        onClose={() => setModalVisible(false)}
+        onAddTask={addTask}
+        onEditTask={editTask}  
+        taskToEdit={taskToEdit}
+      />
       <View style={styles.taskSummary}>
         <TouchableOpacity style={styles.summaryCardCadastradas} onPress={() => navigation.navigate('RegisteredTasksScreen', { tasks, setTasks })}>
           <Text style={styles.summaryText}> Cadastradas</Text>
@@ -111,24 +152,6 @@ const Home = () => {
           <Text style={styles.summaryCount}>{completedTasks}</Text>
         </TouchableOpacity>
       </View>
-
-       
-      <TaskList 
-        tasks={tasks} 
-        toggleTaskCompletion={toggleTaskCompletion} 
-        deleteTask={deleteTask} 
-        openEditModal={openEditModal} 
-      />
-      
-      <TaskModal
-        isVisible={isModalVisible}
-        onClose={() => setModalVisible(false)}
-        onAddTask={addTask}
-        onEditTask={editTask}  
-        taskToEdit={taskToEdit}
-      />
-      
-      <Text style={styles.usernameText}>Olá, {username}!</Text>
     </View>
   );
 };
@@ -138,9 +161,26 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: '#f0f0f0',
-    overflow:'hidden'
- 
   },
+  containerText: {
+    justifyContent: 'center',
+    alignItems: 'center', // Centraliza horizontalmente
+    backgroundColor: '#f0f0f0',
+    marginBottom: 20, // Espaçamento abaixo do texto (opcional)
+    width: '100%', // Faz a largura ocupar o máximo disponível
+  },
+
+  contentSave: {
+    display: 'flex',
+    flexDirection: 'row', // Coloca o input e o botão em linha
+    width: '100%', 
+    justifyContent: 'center',
+    alignItems: 'center', // Alinha os itens verticalmente
+    backgroundColor: '#fff', // Agora o background-color será aplicado
+    padding:5,
+    borderRadius:10
+  },
+  
   taskSummary: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -164,6 +204,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     elevation: 5,
   },
+  input: {
+    flex: 1, // O input vai ocupar o espaço disponível
+    borderColor: 'transparent',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    marginRight: 10, // Adiciona espaço entre o input e o botão
+  },
   summaryText: {
     color: '#fff',
     fontSize: 16,
@@ -175,11 +225,29 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 5,
   },
+ 
+  
+  addButton: {
+    backgroundColor: '#4A90E2',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100, // Define uma largura fixa para o botão
+    height: 50, // Define uma altura fixa para o botão
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
   usernameText: {
     marginTop: 20,
     fontSize: 18,
     color: '#333',
+    textAlign: 'center', // Garante que o texto seja centralizado
   },
+  
 });
 
 export default Home;
